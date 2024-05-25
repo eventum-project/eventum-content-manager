4ab001cae2acb90b04922384ecba470016a093fa
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2024-05-25 19:41:45 +0300

    Update tests for validators

--- eventum_content_manager/tests/test_validators.py
+++ eventum_content_manager/tests/test_validators.py
@@ -5,42 +5,81 @@ from eventum_content_manager.validators import (validate_csv_filename,
                                                 validate_yaml_filename)
 
 
-def test_validate_jinja_filename():
-    validate_jinja_filename('template.jinja')
-    validate_jinja_filename('template.json.jinja')
+@pytest.mark.parametrize(
+    ('filename',),
+    [
+        ('template.jinja', ),
+        ('template.json.jinja', ),
+        ('test_template.json.jinja', ),
+        ('test_template_2.json.jinja', ),
+    ]
+)
+def test_validate_jinja_filename_valid(filename):
+    validate_jinja_filename(filename)
 
+
+@pytest.mark.parametrize(
+    ('filename',),
+    [
+        ('template.json', ),
+        ('test template.jinja', ),
+        ('template', ),
+        ('', ),
+    ]
+)
+def test_validate_jinja_filename_invalid(filename):
     with pytest.raises(ValueError):
-        validate_jinja_filename('template.json')
+        validate_jinja_filename(filename)
 
+
+@pytest.mark.parametrize(
+    ('filename',),
+    [
+        ('config.yaml', ),
+        ('config.yml', ),
+        ('test_config.yml', ),
+        ('test_config2.yaml', ),
+    ]
+)
+def test_validate_yaml_filename_valid(filename):
+    validate_yaml_filename(filename)
+
+
+@pytest.mark.parametrize(
+    ('filename',),
+    [
+        ('config.toml', ),
+        ('test config.yml', ),
+        ('config', ),
+        ('', ),
+    ]
+)
+def test_validate_yaml_filename_invalid(filename):
     with pytest.raises(ValueError):
-        validate_jinja_filename('template')
+        validate_yaml_filename(filename)
 
+
+@pytest.mark.parametrize(
+    ('filename',),
+    [
+        ('sample.csv', ),
+        ('test_sample.csv', ),
+        ('test_sample2.csv', ),
+    ]
+)
+def test_validate_csv_filename_valid(filename):
+    validate_csv_filename(filename)
+
+
+@pytest.mark.parametrize(
+    ('filename',),
+    [
+        ('sample.xls', ),
+        ('test sample.csv', ),
+        ('sample', ),
+        ('', ),
+    ]
+)
+def test_validate_csv_filename_invalid(filename):
     with pytest.raises(ValueError):
-        validate_jinja_filename('')
-
-
-def test_validate_yaml_filename():
-    validate_yaml_filename('config.yaml')
-    validate_yaml_filename('config.yml')
-
-    with pytest.raises(ValueError):
-        validate_yaml_filename('config.toml')
-
-    with pytest.raises(ValueError):
-        validate_yaml_filename('config')
-
-    with pytest.raises(ValueError):
-        validate_yaml_filename('')
-
-
-def test_validate_csv_filename():
-    validate_csv_filename('sample.csv')
-
-    with pytest.raises(ValueError):
-        validate_csv_filename('sample.xls')
-
-    with pytest.raises(ValueError):
-        validate_csv_filename('sample')
-
-    with pytest.raises(ValueError):
-        validate_csv_filename('')
+        validate_csv_filename(filename)
